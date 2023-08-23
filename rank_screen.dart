706bad7ab5dd0c311ea8f6758ee9c2cd73c6c1f1
Jinwon Joung <129395464+JoungJinwon@users.noreply.github.com>
2023-08-23 14:47:14 +0900

    3

--- rank_screen.dart
+++ rank_screen.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class RankPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('메인 홈'),
+        title: Text('랭킹'),
       ),
       body: Center(
-        child: Text('This is the second page.'),
+        child: Text('랭크페이지'),
       ),
     );
   }
