52f91aecccbcc4d9d4a575d1743ea9f2fe92a8a3
Ko Jaehwi <kojaehwi@gmail.com>
2023-09-07 11:25:58 +0900

    hide!

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,8 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyDBL3wIgnErI6GY91aIck2bu8n6R7F7SEI")
+    NSString* mapsApiKey = [[NSProcessInfo processInfo] environment[@"ios_key"];
+    GMSServices.provideAPIKey(mapsApiKey)
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
